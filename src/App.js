0f94c96c28c078a5a2199f6a4b5be9de5937a127
Melissa Solano <m.solanof13@gmail.com>
2023-08-23 13:31:19 -0600

    Agrego detalles para poder buscar por ruta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Asegúrate de incluir Routes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import PersonDetail from './components/PersonDetail';
